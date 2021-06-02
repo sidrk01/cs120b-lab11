a27ec132228afc56d35468f9674e67b0cd3464b1
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-06-02 12:51:22 -0700

    Update ledmatrix.h

--- header/ledmatrix.h
+++ header/ledmatrix.h
@@ -5,25 +5,58 @@
 //Period: 100 ms 
 //------------------------
 
-enum Demo_States {shift};
+enum Demo_States {init, start, start_seq, paddle_out};
 int Demo_Tick(int state) {
 	
 	//Transitions 
 	switch(state){
-		case shift:
-			break;
+		case init:
+			state = start;
+		break;
 		
-		default:
-			state = shift;
-			break;
+		case start:
+			state = start_seq;
+		break;
+		
+		case start_seq:
+			state = paddle_out;
+		break;
+		
+		case paddle_out:
+			state = paddle_out;
+		break;
 	}
 
 	//Actions
 	switch (state){
-	case shift:
-	PORTC = pattern; //Pattern to display 
-        PORTD = row;    // Row(s) displaying pattern
-	break;
+		case init:
+			PORTC = 0xFF;
+		break;
+	
+		case start:
+			PORTD = 0x0F;
+		break;
+		
+		case start_seq:
+			PORTD = 0xF0;
+		break;
+			
+		case paddle_out:
+			if (paddlepos == 0x10){
+				PORTC = 0x38; 
+			} else if (paddlepos == 0x20){
+				PORTC = 0x70;
+			} else if (paddlepos == 0x40){
+				PORTC = 0xE0;
+			} else if (paddlepos == 0x08){
+				PORTC = 0x1C;
+			} else if (paddlepos == 0x04){
+				PORTC = 0x0E;	
+			} else if (paddlepos == 0x02){
+				PORTC = 0x07;
+			}
+			PORTD = 0xFE;
+		break;
 	}
 	
 	return state;	
