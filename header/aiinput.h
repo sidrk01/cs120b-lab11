e50b752eeb28b0c58b9d8e2786c4ba3fb73910fc
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-06-04 15:06:43 -0700

    Update aiinput.h

--- header/aiinput.h
+++ header/aiinput.h
@@ -1,4 +1,74 @@
 #ifndef __AI_INPUT_H__
 #define __AI_INPUT_H__
 
+#define b6 ~PINB & 0x10
+#define b7 ~PINB & 0x20
+
+unsigned char enemypaddlepos = 0x10;
+unsigned long enemyval = 0;
+
+enum paddleCheck2 { enemy_init, enemy_start, enemy_wait, enemy_press, enemy_release };
+
+int Enemy_Input (int state) {
+  switch (state){
+    case enemy_init:
+        state = enemy_start;
+    break;
+      
+    case enemy_start:
+        state = enemy_wait;
+    break;
+      
+    case enemy_wait:
+      if (b6){
+        if (enemypaddlepos != 0x40){
+        state = enemy_press;
+        } else {
+        state = enemy_wait;
+      }
+  } else if (b7){
+    if (paddlepos != 0x20){
+      state = enemy_press;
+    } else {
+      state = enemy_wai;
+    }
+  }
+ break;
+      
+    case enemy_press:
+      state = enemy_release;
+    break;
+      
+    case enemy_release:
+       if (b6){
+        state = enemy_release;
+       } else if (b7){
+        state = enemy_release;
+       } else {
+        state = enemy_wait;
+       }
+    break;
+  }
+  
+  switch (state){
+    case enemy_init:
+    case enemy_start:
+    case enemy_wait:
+    break;
+      
+    case enemy_press:
+      if (b6){
+        if (enemypaddlepos != 0x40){
+          enemypaddlepos = enemypaddlepos << 1;
+        } 
+      } else if (b7){
+        if (enemypaddlepos != 0x02){
+          enemypaddlepos = enemypaddlepos >> 1;
+        } 
+      }
+    break;
+  }
+  
+  return state;
+}
 #endif __AI_INPUT_H__
