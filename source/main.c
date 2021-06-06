563e68dbff1881738dd4fd24607c598b8bda7917
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-06-06 14:59:39 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -12,42 +12,33 @@
 #include "simAVRHeader.h"
 #endif
 
+#define b1 ~PINB & 0x01
+#define b2 ~PINB & 0x02
+#define b3 ~PINB & 0x04
+#define b4 ~PINB & 0x08
+#define b5 ~PINB & 0x10
+#define b6 ~PINB & 0x20
+#define b7 ~PINB & 0x40
+
+unsigned int playerpos = 4; 
+unsigned int aipos = 3;
+
+unsigned char displVal[5];
+unsigned int currrow = 0;
+
+unsigned char ballVal = 0x00;
+unsigned char ballRow = 0x03;
+unsigned char rowDispl = 0x10;
+unsigned int ballCol = 4;
+//unsigned char row =  rows[index]; //Row(s) displaying pattern.
+					//0: display pattern on row 
+					//1: do NOT display pattern on row
+
 #include "../header/timer.h"
 //#include "../header/ledmatrix.h"
 //#include "../header/ballmove.h"
 #include "../header/paddleinput.h"
 //#include "../header/aiinput.h"
-
-#define b1 ~PINB & 0x01
-#define b2 ~PINB & 0x02
-#define b3 ~PINB & 0x40
-#define b4 ~PINB & 0x08
-#define b5 ~PINB & 0x04
-#define b6 ~PINB & 0x10
-#define b7 ~PINB & 0x20
-
-unsigned char xpos = 0x00; 
-unsigned char ypos = 0x00;
-
-unsigned char xpos_index = 0x08;
-unsigned long i = 0;
-
-//write paddle pos 
-unsigned char paddlepos = 0x10;
-//unsigned char rows[3] = {0x03, 0x11, 0x18}
-unsigned char autom = 0x00;
-//unsigned char pattern = 0x80; //LED pattern - 0: LED off; 1: LEDon
-unsigned int index = 1;
-//unsigned char row =  rows[index]; //Row(s) displaying pattern.
-					//0: display pattern on row 
-					//1: do NOT display pattern on row
-unsigned char x_pos_right = 0x00;
-unsigned char x_pos_left = 0x00;
-unsigned char y_pos_up = 0x01;
-unsigned char y_pos_down = 0x00;
-
-unsigned char enemypaddlepos = 0x10;
-unsigned long enemyval = 0;
 
 typedef struct _task {
         signed char state;
