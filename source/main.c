18052c0a1ac426b11bd0ec17ce6ae2a525640caa
sidrk01 <81598654+sidrk01@users.noreply.github.com>
2021-06-06 17:21:05 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: Sidharth Ramkumar (sramk002)
  *  Partner(s) Name: none
  *	Lab Section: 022
- *	Assignment: Lab #11  Exercise #0
+ *	Assignment: Lab #11  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -36,11 +36,320 @@ unsigned int ballCol = 4;
 					//1: do NOT display pattern on row
 
 #include "../header/timer.h"
-#include "../header/ledmatrix.h"
-#include "../header/paddleinput.h"
-#include "../header/aiinput.h"
-#include "../header/ballmove.h"
 
+enum Demo_States { init, output};
+
+int Demo_Tick(int state) {
+	unsigned char bitmask = 0x01;
+	
+	switch(state){
+		case init:
+			state = output;
+		break;
+			
+		case output:
+		break;
+	}
+	
+	switch(state){
+		case output:
+			PORTC = displVal[currow];
+			for (unsigned int i = 0; i < currow; i++){
+				bitmask = bitmask << 1;
+			}
+			PORTD = ~bitmask;
+			
+			currow += 1;
+			if (currow >= 6){
+				currow = 0;
+			}
+		break;
+		
+		default:
+		break;
+	}
+	return state;	
+}
+
+enum PaddleCheck {paddle_init, paddle_wait, press_left, wait_left, press_right, wait_right, press_reset, wait_reset};
+
+int Paddle_Input(int state){
+	unsigned char displ = 0x00;
+	unsigned char bitmask = 0x01;
+
+	switch(state){
+		case paddle_init:
+			state = paddle_wait;
+		break;
+			
+		case paddle_wait:
+			if (b1){
+				state = press_left;
+			} else if (b2){
+				state = press_right;
+			} else if (b3) {
+				state = press_reset;
+			} else {
+				state = paddle_wait;
+			}
+		break;
+			
+		case press_left:
+			if (b1){
+				state = wait_left; 
+			} else if (b2){
+				state = press_right;
+			} else {
+				state = paddle_wait;
+			}
+		break;
+			
+		case wait_left:
+			if (b1){
+				state = wait_left;
+			} else if (b2){
+				state = press_right;
+			} else {
+				state = paddle_wait;
+			}
+		break;
+		
+		case press_right:
+			if (b1){
+				state = press_left;
+			} else if (b2){
+				state = wait_right;
+			} else {
+				state = paddle_wait;
+			}
+		break;
+			
+		case wait_right:
+			if (b1){
+				state = press_left;
+			} else if (b2){
+				state = wait_right;
+			} else {
+				state = paddle_wait;
+			}
+		break;
+		
+		case press_reset:
+			state = wait_reset;
+		break;
+		
+		case wait_reset:
+			if (b3){
+				state = wait_reset;
+			} else {
+				state = paddle_wait;
+			}
+		break;
+	}
+	
+	switch (state){
+		case press_left:
+			if (playerpos <= 5){
+				playerpos += 1;
+			}
+		break;
+		
+		case press_right:
+			if (playerpos >= 2){
+				playerpos -= 1;
+			}
+		break;
+		
+		case press_reset:
+			playerpos = 4;
+			aipos = 3;
+			ballVal = 0x00;
+			ballRow = 0x03;
+			ballCol = 4;
+			rowDispl = 0x10;
+		break;
+		
+		default:
+		break;
+	}
+			
+	for (unsigned int i = 0; i <= playerpos + 1; i++){
+		if (i >= playerpos - 1){
+			displ |= bitmask;
+		}
+		bitmask = bitmask << 1;
+	}
+	displVal[4] = displ;
+			
+	return state;
+}
+
+enum paddleCheck2 { enemy_init };
+
+int Enemy_Input (int state) {
+     
+
+  unsigned char tmpDispl = 0x00;
+  unsigned char bitmask = 0x01;
+  
+ int numRand = rand() % 10;
+ int posLoc = aipos - ballCol;
+ 
+ switch(state){
+  default:
+  
+  if (numRand < 7){
+    if (posLoc > 0){
+      if (aipos >= 2){
+        aipos -= 1;
+      }
+    } else if (posLoc < 0){
+      if (aipos <= 5){
+        aipos += 1;
+      }
+    }
+  } else {
+    if (posLoc > 0 && aipos <= 5){
+      aipos += 1;
+    } else if (posLoc < 0 && aipos >= 2){
+      aipos -= 1;
+    } else {
+      if (aipos <= 5){
+        aipos += 1;
+      } else {
+        aipos -= 1;
+      }
+    }
+  }
+  
+  for (unsigned int i = 0; i <= aipos + 1; i++){
+    if ( i >= aipos - 1){
+      tmpDispl |= bitmask;
+    }
+    bitmask = bitmask << 1;
+  }
+  
+  displVal[0] = tmpDispl;
+  
+  break;
+      
+  }
+  
+  return state;
+}
+enum Ball_Movement { ball_init, ball_move};
+
+void rowSwitch(){
+    if (ballVal & 0x01){
+        ballVal = ballVal & 0xFE;
+    } else {
+        ballVal = ballVal | 0x01;
+    }
+}
+
+void colSwitch(){
+    if (ballVal & 0x02){
+        ballVal = ballVal & 0xFD;
+    } else {
+        ballVal = ballVal | 0x02;
+    }
+}
+
+int Ball_Tick(int state){
+    
+    switch (state){
+        case ball_init:
+             state = ball_move;
+        break;
+            
+        case ball_move:
+        break;
+    }
+    
+    switch (state){
+        case ball_move:
+            if (ballVal & 0x02){
+                if (ballRow < 3){
+                    ballRow += 1;
+                } else {
+                    if (playerpos - 1 == ballCol){
+                        colSwitch();
+                        rowSwitch();
+                        ballRow -= 1;
+                    } else if (playerpos + 1 == ballCol ){
+                        colSwitch();
+                        rowSwitch();
+                        ballRow -= 1;
+                    } else if (playerpos == ballCol){
+                        colSwitch(); 
+                        ballRow -= 1;
+                    } else {
+                        ballRow = 0x01;
+                        ballCol = 4;
+                        rowDispl = 0x08;
+                        ballVal = 0x03;
+                        
+                    }
+                }
+            } else {
+                if (ballRow > 1){
+                    ballRow -= 1;
+                } else {
+                    if (aipos - 1 == ballCol){
+                        colSwitch();
+                        rowSwitch();
+                        ballRow += 1;
+                    } else if (aipos + 1 == ballCol){
+                        colSwitch();
+                        rowSwitch();
+                        ballRow += 1;
+                    } else if (aipos == ballCol){
+                        colSwitch();
+                        ballRow += 1;
+                    } else {
+                        ballRow = 0x03;
+                        ballCol = 4;
+                        rowDispl = 0x10;
+                        ballVal = 0x00;
+                        
+                    }
+                }
+            }
+            
+            if (ballVal & 0x01){
+                if (rowDispl <= 0x40){
+                    rowDispl = rowDispl << 1;
+                    ballCol += 1;
+                } else {
+                    rowSwitch();
+                    rowDispl = rowDispl >> 1;
+                    ballCol -= 1;
+                }
+            } else {
+                if (rowDispl >= 0x02){
+                    rowDispl = rowDispl >> 1;
+                    ballCol -= 1;
+                } else {
+                    rowSwitch();
+                    rowDispl = rowDispl << 1;
+                    ballCol += 1;
+                }
+            }
+            break;
+           
+        default:
+           break;
+    }
+        for (unsigned int i = 1; i <= 3; i++){
+            if (i == ballRow){
+                displVal[i] = rowDispl;
+            } else {
+                displVal[i] = 0;
+            }
+        }
+      return state;
+         
+}
 
 typedef struct _task {
         signed char state;
